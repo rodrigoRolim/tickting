bc68b73aabd99d3791645675bd19ff8a34206c82
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-14 14:55:44 -0300

    create new routes

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-
+  res.send('hi')
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import{ body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validations-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  const { email, password } = req.body;
+router.post('/api/users/signup', [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must between and 20 characters')
+  ],(req: Request, res: Response) => {
+  
+  const errors = validationResult(req);
 
-  if (!email || typeof email !== 'string') {
-    res.status(400).send('Provide a valid email')
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
   }
+  console.log('Creating User...')
+  throw new DatabaseConnectionError();
+  
+  res.send({})
 });
 
 export { router as signupRouter };
